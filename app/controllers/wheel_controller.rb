c29c000f75db98d84b92e66ffdc4bc3e592256c4
Adrian Romero <aromero@manas.com.ar>
2010-05-05 16:33:07 -0300

    Wheel draw, added images into pdf

--- app/controllers/wheel_controller.rb
+++ app/controllers/wheel_controller.rb
@@ -1,13 +1,15 @@
 require 'rvg/rvg'
+require 'rubygems'
+require 'pdf/writer'
 include Magick
 
-RVG::dpi = 90
+RVG::dpi = 72
 
 class WheelController < ApplicationController
   
   @@angle_separation = 5
   @@angle_modifier = 2
-  @@initial_radius = 10.4.cm
+  @@initial_radius = 10.5.cm
   @@row_separation = 0.2.cm
   @@colors = ['red', 'green', 'blue']
   @@values_font_size = 14
@@ -15,6 +17,9 @@ class WheelController < ApplicationController
   @@left_size = 2.5.cm
   @@right_size = 1.cm
   @@field_cover_height = 0.8.cm
+  @@width = 22.cm
+  @@height = 22.cm
+  
   
   before_filter :find_wheel, :only => [:draw]
   
@@ -38,60 +43,73 @@ class WheelController < ApplicationController
   end
   
   def draw
-    global_dy = 0
+    pdf = PDF::Writer.new
     
-    rvg = RVG.new(21.cm,4 * @@initial_radius * 2 + 1.cm) do |canvas|
-       canvas.background_fill = 'white'
-       
-       # Draw wheel rows
-       @wheel.rows.sort.each_with_index do |row, i|
-         canvas.g.translate(@@initial_radius + 0.1.cm, @@initial_radius + 0.1.cm + global_dy) do |group|
-           left_radius = @@initial_radius - i * @@left_size - i * @@row_separation
-           right_radius = @@initial_radius - i * @@right_size - i * @@row_separation
+    # Draw wheel rows
+    @wheel.rows.sort.each_with_index do |row, i|
+      rvg = RVG.new(@@width, @@height) do |canvas|
+        canvas.background_fill = 'white'
+        
+        canvas.g.translate(@@initial_radius + 0.1.cm, @@initial_radius + 0.1.cm) do |group| 
+          left_radius = @@initial_radius - i * @@left_size - i * @@row_separation
+          right_radius = @@initial_radius - i * @@right_size - i * @@row_separation
+          
+          circle(group, left_radius, right_radius)
+          
+          indexes = (0..row.values.count-1).map{|z| z - row.values.count/2}.reverse
            
-           circle(group, left_radius, right_radius)
-           
-           indexes = (0..row.values.count-1).map{|z| z - row.values.count/2}.reverse
-           
-           row.values.sort.each_with_index do |value, j|
-             angle = (@@angle_separation + i * @@angle_modifier) * indexes[j]
-             angle_rad = to_rad angle
-             
-             dx = - (left_radius - @@row_separation) * Math.cos(angle_rad)
-             dy = - (left_radius - @@row_separation) * Math.sin(angle_rad)
-             group.text(dx, dy, value.value).rotate(angle).styles(:text_anchor =>'start', :font_size => @@values_font_size,
-               :font_family => 'monaco', :fill => 'black')
-             
-             dx = (right_radius - @@row_separation) * Math.cos(angle_rad)
-             dy = (right_radius - @@row_separation) * Math.sin(angle_rad)
-             group.text(dx, dy, value.code).rotate(angle).styles(:text_anchor => 'end', :font_size => @@codes_font_size, 
-               :font_family => 'helvetica', :fill => 'black')
-           end  
-           # max_value = row.values.map{|v| v.value.length}.max
-           # left_margin += max_value * @@ppc
-         end
-         
-         global_dy += @@initial_radius * 2         
-       end
-       
-       # Draw cover
-        canvas.g.translate(@@initial_radius + 0.1.cm, @@initial_radius + 0.1.cm + global_dy) do |g|
-          g.circle(@@initial_radius).styles(:stroke => 'black', :fill => 'transparent')
-          @wheel.rows.each_with_index do |row, i|
-            dx = - @@initial_radius + i * @@left_size + i * @@row_separation + 0.1.cm
-            dy = - @@field_cover_height / 2 - 0.1.cm
-            g.rect(@@left_size, @@field_cover_height, dx, dy).styles(:fill => 'transparent', :stroke => 'black')
-            
-            dx = @@initial_radius - i * @@right_size - i * @@row_separation - @@right_size - 0.1.cm
-            g.rect(@@right_size, @@field_cover_height, dx, dy).styles(:fill => 'transparent', :stroke => 'black')             
-          end
-        end
-     end
-     
-     img = rvg.draw
-     img.format = "PNG"
+          row.values.sort.each_with_index do |value, j|
+            angle = (@@angle_separation + i * @@angle_modifier) * indexes[j]
+            angle_rad = to_rad angle
 
-     send_data(img.to_blob, :disposition => 'inline', :type => 'image/png')
+            dx = - (left_radius - @@row_separation) * Math.cos(angle_rad)
+            dy = - (left_radius - @@row_separation) * Math.sin(angle_rad)
+            group.text(dx, dy, value.value).rotate(angle).styles(:text_anchor =>'start', :font_size => @@values_font_size,
+             :font_family => 'monaco', :fill => 'black')
+
+            dx = (right_radius - @@row_separation) * Math.cos(angle_rad)
+            dy = (right_radius - @@row_separation) * Math.sin(angle_rad)
+            group.text(dx, dy, value.code).rotate(angle).styles(:text_anchor => 'end', :font_size => @@codes_font_size, 
+             :font_family => 'helvetica', :fill => 'black')
+          end
+          
+          group.circle(0.1.cm).styles(:fill => 'black')
+        end
+      end
+            
+      img = rvg.draw
+      img.format = "JPG"
+      
+      pdf.add_image(img.to_blob, 0 ,0)
+      pdf.start_new_page
+    end
+    
+    # Draw wheel cover
+    rvg_cover = RVG.new(@@width, @@height) do |canvas|
+      canvas.background_fill = 'white'
+      
+      canvas.g.translate(@@initial_radius + 0.1.cm, @@initial_radius + 0.1.cm) do |g|
+        
+        g.circle(0.1.cm).styles(:fill => 'black')
+    
+        g.circle(@@initial_radius).styles(:fill => 'transparent', :stroke => 'black')
+        @wheel.rows.each_with_index do |row, i|
+          dx = - @@initial_radius + i * @@left_size + i * @@row_separation + 0.1.cm
+          dy = - @@field_cover_height / 2 - 0.1.cm
+          g.rect(@@left_size, @@field_cover_height, dx, dy).styles(:fill => 'transparent', :stroke => 'black')
+          
+          dx = @@initial_radius - i * @@right_size - i * @@row_separation - @@right_size - 0.1.cm
+          g.rect(@@right_size, @@field_cover_height, dx, dy).styles(:fill => 'transparent', :stroke => 'black')
+        end          
+      end
+    end
+    
+    img_cover = rvg_cover.draw
+    img_cover.format = "JPG"
+    
+    pdf.add_image(img_cover.to_blob, 0, 0)
+    
+    send_data(pdf.render , :disposition => 'inline', :type => 'application/pdf', :filename => "wheel_#{@wheel.name}.pdf")
   end
   
   private
