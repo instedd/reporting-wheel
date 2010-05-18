d34497dd552e9235d69a1e792aa0610e9f62668e
Adrian Romero <aromero@manas.com.ar>
2010-05-18 12:46:16 -0300

    Added script to run delayed jobs daemon

--- config/environment.rb
+++ config/environment.rb
@@ -24,6 +24,7 @@ Rails::Initializer.run do |config|
   config.gem "rmagick", :lib => "rmagick"
   config.gem "pdf-writer", :lib => 'pdf/writer'
   config.gem 'collectiveidea-delayed_job', :lib => 'delayed_job', :source => 'http://gems.github.com'
+  config.gem 'daemons'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
