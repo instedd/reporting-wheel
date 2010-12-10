8e45f6e2d720601412571b51d4ddbb483e04660d
Adrian Romero <aromero@manas.com.ar>
2010-12-10 16:51:54 -0300

    Partial UI for new/edit wheel (missing: style to tabs, print configuration)
    --HG--
    branch : ui-refactor

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,9 +7,41 @@ function remove_fields(link) {
   $(link).closest(".fields").hide();
 }
 
+function remove_value(link) {
+  $(link).prev("input[type=hidden]").val("1");
+  $(link).closest("span").hide();
+}
+
+function remove_label(link) {
+  $(link).prev("input[type=hidden]").val("1");
+  $(link).closest(".label_box").hide();
+}
+
+function add_label(link, association, content) {
+	var new_id = new Date().getTime();
+	var id_regexp = new RegExp("new_" + association, "g");
+	var label_name_regexp = new RegExp("__LABEL__", "g");
+	var label_name = $("#label_name").val();
+	content = content.replace(id_regexp, new_id).replace(label_name_regexp, label_name);
+	$("#labels").append(content);
+	$("#label_name").val("");
+	do_watermarks();
+}
+
+function add_value(link, association, content) {
+	var new_id = new Date().getTime();
+	var id_regexp = new RegExp("new_" + association, "g");
+	var value_name_regexp = new RegExp("__VALUE__", "g");
+	var value_name = $(link).prev("input[type=text]").val();
+	content = content.replace(id_regexp, new_id).replace(value_name_regexp, value_name);
+	$(link).closest(".label_box").children(".values").append(content);
+	$(link).prev("input[type=text]").val("");
+	do_watermarks();
+}
+
 function add_fields(link, association, content) {
   var new_id = new Date().getTime();
-  var regexp = new RegExp("new_" + association, "g")
+  var regexp = new RegExp("new_" + association, "g");
   $(link).parent().before(content.replace(regexp, new_id));
 }
 
